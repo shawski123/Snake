88e6175eaef817a9a9c9bd85882a85380a2d0254
shawski123 <sean2me2016@gmail.com>
2025-07-29 15:38:26 -0700

    Basically done but could add UI and a map in the future

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,27 @@
 #include <iostream>
 #include <random>
 
-struct Player {
-    static constexpr float speed = 300.f;
+enum PlayerBody {
+    head = 0
 };
 
-void characterMovement(sf::Vector2f& movement, float speed, float deltaTime, sf::Sprite& player, sf::Vector2f& currentDirection);
+enum Directions {
+    left = 0,
+    right = 1,
+    up = 2,
+    down = 3
+};
+
+struct Player {
+    static constexpr float speed = 250.f;
+    bool directions[4] = { false, false, false, false };
+};
+
+void characterMovement(sf::Vector2f& movement, float speed, float deltaTime, std::vector<sf::Sprite>& character, sf::Vector2f& currentDirection, Player& moveDirec);
 sf::Sprite createPlayeSprite(sf::Texture& texture, const sf::Vector2f position);
 float getRandX(const sf::RenderWindow& window);
 float getRandY(const sf::RenderWindow& window);
+sf::FloatRect getMouthHitbox(const sf::Sprite& head, const sf::Vector2f& direction);
 
 int main()
 {
@@ -23,6 +36,9 @@ int main()
     int count = 0;
     bool appleEaten = false;
     sf::Vector2f randNums;
+    std::vector<sf::Sprite> character;
+    std::vector<sf::Vector2f> positionHistory;
+    Player moveDirec;
 
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Snake Game", sf::Style::Default, sf::State::Windowed, settings);
     window.setFramerateLimit(144);
@@ -36,29 +52,45 @@ int main()
     else {
         return -1;
     }
+    
+    sf::Texture appleTexture;
+    if (appleTexture.loadFromFile("resources/character/apple_alt_32-export.png")) {
+        std::cout << "Texture loaded successfully!" << std::endl;
+    }
+    else {
+        return -1;
+    }
 
+    //Font
     sf::Font font("resources/arial-font/arial.ttf");
     sf::Text score(font);
     score.Bold;
     score.setCharacterSize(24);
     score.setFillColor(sf::Color::White);
 
-    sf::CircleShape entity(40);
-    entity.setFillColor(sf::Color::Red);
-    entity.setPosition({200, window.getSize().y / 2.f});
+    //Apple
+    sf::Sprite entity(appleTexture);
+    entity.setPosition({ 200, window.getSize().y / 2.f });
 
+    //Snake head
     sf::Vector2f playerDefaultPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
     sf::Sprite player = createPlayeSprite(texture, playerDefaultPosition);
-
-    player.setTextureRect(sf::IntRect({0,0} , {42,42}));
+    player.setTextureRect(sf::IntRect({ 0,0 }, { 42,42 }));
     player.setOrigin({ player.getLocalBounds().size.x / 2.f, player.getLocalBounds().size.y / 2.f });
+    character.push_back(player);
+
+    //Start with 1 body
+    sf::Sprite newSegment(texture);
+    newSegment.setTextureRect(sf::IntRect({ 84,84 }, { 42,42 }));
+    newSegment.setOrigin({ newSegment.getLocalBounds().size.x / 2.f, newSegment.getLocalBounds().size.y });
+    newSegment.setPosition(character.back().getPosition()); // Initial attach
+    character.push_back(newSegment);
+    sf::FloatRect boundBoxBody = newSegment.getGlobalBounds();
     //-----------------------------LOAD-----------------------------
 
     //GAME LOOP
     while (window.isOpen())
     {
-        score.setString("Score: " + std::to_string(count));
-
         float deltaTime = clock.restart().asSeconds();
 
         while (const std::optional event = window.pollEvent())
@@ -70,51 +102,112 @@ int main()
 
         }
 
-        sf::Vector2f movement(0.f, 0.f);
-
-        characterMovement(movement, Player::speed, deltaTime, player, currentDirection);
-
-        sf::FloatRect otherBox = entity.getGlobalBounds();
-        sf::FloatRect boundingBox = player.getGlobalBounds();
-        
-        if (const std::optional intersection = boundingBox.findIntersection(otherBox))
-        {
-            randNums.x = getRandX(window);
-            randNums.y = getRandY(window);
-
-            std::cout << "Collision" << std::endl;
-            count++;
-            entity.setPosition(randNums);
+        std::vector<sf::Vector2f> previousPositions;
+        for (const auto& segment : character) {
+            previousPositions.push_back(segment.getPosition());
         }
 
+        score.setString("Score: " + std::to_string(count));
+
+        sf::Vector2f movement(0.f, 0.f);
+        characterMovement(movement, Player::speed, deltaTime, character, currentDirection, moveDirec);
+
+        for (size_t i = 1; i < character.size(); ++i) {
+            character[i].setPosition(previousPositions[i - 1]);
+            character[i].setRotation(character[i - 1].getRotation());
+        }
+
+        positionHistory.push_back(character[head].getPosition());
+
+        // Limit position history to avoid infinite growth
+        const std::size_t delayPerSegment = 10.5;
+        const std::size_t maxHistory = delayPerSegment * character.size();
+        if (positionHistory.size() > maxHistory) {
+            positionHistory.erase(positionHistory.begin(), positionHistory.begin() + (positionHistory.size() - maxHistory));
+        }
+
+        // Move body parts using position history
+        for (std::size_t i = 1; i < character.size(); ++i) {
+            std::size_t index = positionHistory.size() - i * delayPerSegment;
+            if (index < positionHistory.size()) {
+                character[i].setPosition(positionHistory[index]);
+            }
+        }
+
+        sf::FloatRect boundingBox = getMouthHitbox(character[head], currentDirection);
+;
+        sf::FloatRect apple = entity.getGlobalBounds();
+
+        // Collision with apple
+        if (const std::optional intersection = boundingBox.findIntersection(apple)) {
+            sf::Vector2f randPos{ getRandX(window), getRandY(window) };
+            entity.setPosition(randPos);
+
+            sf::Sprite newSegment(texture);
+            newSegment.setTextureRect(sf::IntRect({ 84,84 }, { 42,42 }));
+            newSegment.setOrigin({ newSegment.getLocalBounds().size.x / 2.f, newSegment.getLocalBounds().size.y });
+            newSegment.setPosition(character.back().getPosition()); // Initial attach
+            character.push_back(newSegment);
+            sf::FloatRect boundBoxBody = newSegment.getGlobalBounds();
+
+            count++;
+            std::cout << "Collision - segments: " << character.size() << "\n";
+        }
+
+        for (std::size_t i = 3; i < character.size(); ++i) {
+            sf::FloatRect bodyPartBounds = character[i].getGlobalBounds();
+            if (const std::optional intersection = boundingBox.findIntersection(bodyPartBounds)) {
+                playerAlive = false;
+                std::cout << "Player dead!\n";
+                break;
+            }
+        }
+
+        // Draw
         window.clear();
-        window.draw(player);
+        if (playerAlive == true) {
+            for (const auto& part : character)
+                window.draw(part);
+        }
         window.draw(entity);
         window.draw(score);
         window.display();
     }
 }
 
-void characterMovement(sf::Vector2f& movement, float speed, float deltaTime, sf::Sprite& player, sf::Vector2f& currentDirection) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+void characterMovement(sf::Vector2f& movement, float speed, float deltaTime, std::vector<sf::Sprite>& character, sf::Vector2f& currentDirection, Player& moveDirec) {
+
+    if (!moveDirec.directions[right] && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
         currentDirection = { -1.f, 0.f };
-        player.setRotation(sf::degrees(90));
+        character[head].setRotation(sf::degrees(90));
+        moveDirec.directions[left] = true;
+        moveDirec.directions[up] = false;
+        moveDirec.directions[down] = false;
     }
-    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+    else if(!moveDirec.directions[left] && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
         currentDirection = { 1.f, 0.f };
-        player.setRotation(sf::degrees(-90));
+        character[head].setRotation(sf::degrees(-90));
+        moveDirec.directions[right] = true;
+        moveDirec.directions[up] = false;
+        moveDirec.directions[down] = false;
     }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+    else if (!moveDirec.directions[down] && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
         currentDirection = { 0.f, -1.f };
-        player.setRotation(sf::degrees(180));
+        character[head].setRotation(sf::degrees(180));
+        moveDirec.directions[up] = true;
+        moveDirec.directions[left] = false;
+        moveDirec.directions[right] = false;
     }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+    else if (!moveDirec.directions[up] && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
         currentDirection = { 0.f, 1.f };
-        player.setRotation(sf::degrees(0));
+        character[head].setRotation(sf::degrees(0));
+        moveDirec.directions[down] = true;
+        moveDirec.directions[left] = false;
+        moveDirec.directions[right] = false;
     }
 
     movement = currentDirection * speed;
-    player.move(movement * deltaTime);
+    character[head].move(movement * deltaTime);
 }
 
 sf::Sprite createPlayeSprite(sf::Texture& texture, const sf::Vector2f position) {
@@ -126,12 +219,38 @@ sf::Sprite createPlayeSprite(sf::Texture& texture, const sf::Vector2f position) 
 
 float getRandX(const sf::RenderWindow& window) {
     static std::mt19937 rng(std::random_device{}());
-    std::uniform_real_distribution<float> dist(0.f, static_cast<float>(window.getSize().x - 80));
+    std::uniform_real_distribution<float> dist(0.f, static_cast<float>(window.getSize().x - 42));
     return dist(rng);
 }
 
 float getRandY(const sf::RenderWindow& window) {
     static std::mt19937 rng(std::random_device{}());
-    std::uniform_real_distribution<float> dist(0.f, static_cast<float>(window.getSize().y - 80));
+    std::uniform_real_distribution<float> dist(0.f, static_cast<float>(window.getSize().y - 42));
     return dist(rng);
+}
+
+sf::FloatRect getMouthHitbox(const sf::Sprite& head, const sf::Vector2f& direction) {
+    const float mouthWidth = 8.f;
+    const float mouthHeight = 8.f;
+
+    sf::Vector2f center = head.getPosition();
+
+    sf::Vector2f mouthOffset;
+
+    // Offset the hitbox to the tip of the mouth depending on direction
+    if (direction.x == -1.f) {          // Left
+        mouthOffset = { -21.f, 0.f };
+    }
+    else if (direction.x == 1.f) {    // Right
+        mouthOffset = { 21.f, 0.f };
+    }
+    else if (direction.y == -1.f) {   // Up
+        mouthOffset = { 0.f, -21.f };
+    }
+    else if (direction.y == 1.f) {    // Down
+        mouthOffset = { 0.f, 21.f };
+    }
+
+    sf::Vector2f topLeft = center + mouthOffset - sf::Vector2f(mouthWidth / 2.f, mouthHeight / 2.f);
+    return sf::FloatRect(topLeft, { mouthWidth, mouthHeight });
 }
